aec6bba347de8ee3393fc7dc14b1dd4bd24b6ab9
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-04-26 15:28:29 -0400

    Add Directories static class in Data namespace
    A new static class named Directories has been added to the Data namespace. This class is responsible for creating and getting the root and logs directories paths in the application's base directory.

---
+++ Warehouse.Server/Data/Directories.cs
@@ -0,0 +1,9 @@
+﻿namespace PIMA.Data;
+
+public static class Directories
+{
+    public static string Root { get; } =
+        Directory.CreateDirectory(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "data")).FullName;
+
+    public static string Logs { get; } = Directory.CreateDirectory(Path.Combine(Root, "logs")).FullName;
+}
