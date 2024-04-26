2ca93620a6477f9f7091090b7c2e1c8d056a0605
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-04-26 15:27:55 -0400

    Add ApplicationController with GET endpoint
    The commit adds a new ApplicationController to the Warehouse.Server. This controller includes a single GET endpoint (/api) which returns the generated application data.

---
+++ Warehouse.Server/Controllers/ApplicationController.cs
@@ -0,0 +1,17 @@
+﻿// LFInteractive LLC. 2021-2024
+
+using Microsoft.AspNetCore.Mvc;
+using PIMA.Data;
+
+namespace PIMA.Controllers;
+
+[Route("/api")]
+[ApiController]
+public class ApplicationController : ControllerBase
+{
+    [HttpGet()]
+    public IActionResult Get()
+    {
+        return Ok(ApplicationData.GenerateApplicationData());
+    }
+}
