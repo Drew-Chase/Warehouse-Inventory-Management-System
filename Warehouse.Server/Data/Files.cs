c511c19269cbd9e82ce80b126dd65a9835ebf2de
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-04-26 15:28:37 -0400

    Add Files.cs in Data directory
    A new file named Files.cs has been added to the Data directory, within the Warehouse Server. This file contains static properties for referencing application configuration and various log files, utilizing standard paths from the Directories class as the base.

---
+++ Warehouse.Server/Data/Files.cs
@@ -0,0 +1,11 @@
+﻿namespace PIMA.Data;
+
+using static PIMA.Data.Directories;
+
+public static class Files
+{
+    public static string ApplicationConfiguration { get; } = Path.Combine(Root, "application.json");
+    public static string LatestLog { get; } = Path.Combine(Logs, "latest.log");
+    public static string ErrorLog { get; } = Path.Combine(Logs, "error.log");
+    public static string DebugLog { get; } = Path.Combine(Logs, "debug.log");
+}
