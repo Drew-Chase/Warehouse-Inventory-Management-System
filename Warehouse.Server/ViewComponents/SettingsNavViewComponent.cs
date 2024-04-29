8cb4fb1f19965507e01a874efc73c09973e19c6e
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-04-29 16:22:35 -0400

    Add SettingsNavViewComponent in Warehouse.Server
    A new file, SettingsNavViewComponent.cs, has been added to the Warehouse.Server project. This includes the definition for a SettingsNav record and a class, SettingsNavViewComponent which returns a view of SettingsNav. This is used for displaying the settings navigation.

---
+++ Warehouse.Server/ViewComponents/SettingsNavViewComponent.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Warehouse.ViewComponents;
+
+public record SettingsNav(string Tab);
+
+public class SettingsNavViewComponent : ViewComponent
+{
+    public IViewComponentResult Invoke(SettingsNav options)
+    {
+        return View((object)options);
+    }
+}
