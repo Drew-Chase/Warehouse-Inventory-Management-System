4550bd73ec48a76a7cbd1ee9749f957ec091ac38
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-04-29 10:53:44 -0400

    Add NavbarViewComponent in warehouse server
    A new ViewComponent file called NavbarViewComponent.cs is created in the warehouse server. This component contains implementation to render the navigation bar according to given options.

---
+++ Warehouse.Server/ViewComponents/NavbarViewComponent.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Warehouse.ViewComponents;
+public record navbar(string Page);
+public class NavbarViewComponent : ViewComponent
+{
+    public IViewComponentResult Invoke(navbar options)
+    {
+        return View((object)options);
+    }
+}
