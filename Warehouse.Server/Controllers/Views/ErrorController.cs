1fab393630f10f06792a38a49b24b8745cdd0d6f
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-04-29 16:21:48 -0400

    Add ErrorController in Warehouse.Server
    A new ErrorController has been added to the Warehouse.Server project. This controller is primarily responsible for handling and displaying errors, with a specific route set up for error codes and messages.

---
+++ Warehouse.Server/Controllers/Views/ErrorController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Warehouse.Controllers.Views;
+
+public record ErrorViewModel(int Code, string? Message);
+
+public class ErrorController : Controller
+{
+    [Route("/error/{code:int}")]
+    public IActionResult Index([FromRoute] int code, [FromQuery] string? message = null)
+    {
+        ViewData["title"] = $"Error {code}";
+        return View("Error", new ErrorViewModel(code, message));
+    }
+}
