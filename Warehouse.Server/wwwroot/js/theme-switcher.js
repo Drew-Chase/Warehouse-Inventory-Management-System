e44302630c99749cb05fee2e7804497c149847ac
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-04-29 09:19:12 -0400

    Add theme switcher functionality
    Two new JavaScript files, theme-switcher.js and theme-switcher.min.js, have been added to the project. These handle theme-switching function, allowing users to change between dark and light themes. The chosen theme is stored in local storage, ensuring that user's theme preference is retained across sessions.

---
+++ Warehouse.Server/wwwroot/js/theme-switcher.js
@@ -0,0 +1,27 @@
+﻿const themes = {
+    dark: "dark",
+    light: "light"
+}
+
+const link = $("link#color-theme")
+
+function changeTheme(theme) {
+    link.attr("href", `/css/${theme}.css`)
+    localStorage.setItem("theme", theme)
+}
+
+function reloadTheme() {
+    const theme = localStorage.getItem("theme")
+    if (theme) {
+        changeTheme(theme)
+    } else {
+        changeTheme(themes.light)
+    }
+}
+
+window.onload = reloadTheme
+window.changeTheme = changeTheme;
+window.reloadTheme = reloadTheme;
+window.themes = themes;
+
+export {themes, changeTheme, reloadTheme}
