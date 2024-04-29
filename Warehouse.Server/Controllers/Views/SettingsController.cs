85963f73ba14748c82d4d5ebc555191aa5bf0cd0
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-04-29 16:22:09 -0400

    Add SettingsController to the Warehouse Server
    A new SettingsController was created for the Warehouse.Server. This controller includes two actions: Index and Account. These actions return views for the general settings page and the account settings page respectively.

---
+++ Warehouse.Server/Controllers/Views/SettingsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Warehouse.Controllers.Views;
+
+[Route("/settings")]
+public class SettingsController : Controller
+{
+    public IActionResult Index()
+    {
+        ViewData["title"] = "Settings";
+        return View();
+    }
+
+    [Route("account")]
+    public IActionResult Account()
+    {
+        ViewData["title"] = "Account Settings";
+        return View();
+    }
+}
