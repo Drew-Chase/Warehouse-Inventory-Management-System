61fa9b59ea3f60e6ccdc2512fcebc9c1dac7a111
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-04-29 16:22:01 -0400

    Add HomeController with several routes
    Added a HomeController to the project in the Controllers folder. This new controller includes routes to "purchases", "inventory", and "vendors". Each route returns a View and sets a title for that View via ViewData.

---
+++ Warehouse.Server/Controllers/Views/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Warehouse.Controllers.Views;
+
+[Route("/")]
+public class HomeController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    [Route("purchases")]
+    public IActionResult Purchases()
+    {
+        ViewData["title"] = "Purchases";
+        return View();
+    }
+
+    [Route("inventory")]
+    public IActionResult Inventory()
+    {
+        ViewData["title"] = "Inventory";
+        return View();
+    }
+
+    [Route("vendors")]
+    public IActionResult Vendors()
+    {
+        ViewData["title"] = "Vendors";
+        return View();
+    }
+}
