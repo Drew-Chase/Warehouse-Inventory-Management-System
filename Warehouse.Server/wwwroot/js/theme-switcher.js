f0cc9dc37969361cc690ce775a2afbc8f60cc8ca
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-04-29 16:26:19 -0400

    Refactor theme-switcher.js to TypeScript
    The JavaScript file theme-switcher.js has been refactored into TypeScript to improve maintainability and robustness. TypeScript file, theme-switcher.ts, with type annotations has been added. Also, existing JavaScript files have been adjusted to accomodate these changes.

--- Warehouse.Server/wwwroot/js/theme-switcher.js
+++ Warehouse.Server/wwwroot/js/theme-switcher.js
@@ -1,27 +1,22 @@
-﻿const themes = {
-    dark: "dark",
-    light: "light"
-}
-
-const link = $("link#color-theme")
-
+var themes;
+(function (themes) {
+    themes["dark"] = "dark";
+    themes["light"] = "light";
+})(themes || (themes = {}));
+const link = $("link#color-theme");
 function changeTheme(theme) {
-    link.attr("href", `/css/${theme}.css`)
-    localStorage.setItem("theme", theme)
+    link.attr("href", `/css/${theme}.css`);
+    localStorage.setItem("theme", theme);
 }
-
 function reloadTheme() {
-    const theme = localStorage.getItem("theme")
+    const theme = localStorage.getItem("theme");
     if (theme) {
-        changeTheme(theme)
-    } else {
-        changeTheme(themes.light)
+        changeTheme(theme);
+    }
+    else {
+        changeTheme(themes.light);
     }
 }
-
-window.onload = reloadTheme
-window.changeTheme = changeTheme;
-window.reloadTheme = reloadTheme;
-window.themes = themes;
-
-export {themes, changeTheme, reloadTheme}
+window.onload = reloadTheme;
+export { themes, changeTheme, reloadTheme };
+//# sourceMappingURL=theme-switcher.js.map
---
+++ Warehouse.Server/wwwroot/ts/theme-switcher.ts
@@ -0,0 +1,24 @@
+﻿enum themes {
+    dark = "dark",
+    light = "light"
+}
+
+const link:JQuery<HTMLLinkElement> = $("link#color-theme")
+
+function changeTheme(theme: themes) {
+    link.attr("href", `/css/${theme}.css`)
+    localStorage.setItem("theme", theme)
+}
+
+function reloadTheme() {
+    const theme = localStorage.getItem("theme") as themes
+    if (theme) {
+        changeTheme(theme)
+    } else {
+        changeTheme(themes.light)
+    }
+}
+
+window.onload = reloadTheme
+
+export {themes, changeTheme, reloadTheme}
