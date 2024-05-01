0ddc27e4996ddebca399793ecc02bb54541fd993
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-05-01 15:21:21 -0400

    Add SelectorViewComponent in Warehouse.Server
    This commit includes the addition of the SelectorViewComponent class in the Warehouse.Server project. The component receives a SelectorViewModel, then determines the selected option and index which are used to generate the view.

---
+++ Warehouse.Server/ViewComponents/SelectorViewComponent.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Warehouse.ViewComponents;
+
+public record SelectorOption(string Text, string Value, bool Selected = false);
+
+public record SelectorViewModel(string Title, bool HasSearch, SelectorOption[] Options, int SelectedIndex = -1);
+
+public class SelectorViewComponent : ViewComponent
+{
+    public IViewComponentResult Invoke(SelectorViewModel model)
+    {
+        SelectorOption? selectedOption = model.Options.FirstOrDefault(option => option.Selected);
+        int selectedIndex = selectedOption != null ? Array.IndexOf(model.Options, selectedOption) : model.SelectedIndex;
+        return View(model with { SelectedIndex = selectedIndex });
+    }
+}
