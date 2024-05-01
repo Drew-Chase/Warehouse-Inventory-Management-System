e74e93b0bd36cd4a4a0796bd37e592fdd848fe28
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-05-01 15:27:54 -0400

    Add new settings related JavaScript and TypeScript files
    Introduced new JavaScript and TypeScript files under Warehouse.Server/wwwroot for managing themes and settings. These files allow changing the application's theme, with options for saving the chosen theme and resetting theme settings.

---
+++ Warehouse.Server/wwwroot/ts/settings.ts
@@ -0,0 +1,27 @@
+﻿import {themes, changeTheme, currentTheme} from "./theme-switcher.js";
+
+const themeSelector = $(".selector#theme");
+const saveButton = $("#save-button");
+const resetButton = $("#reset-button")
+
+// Stores the temp variable
+let tempTheme: themes = currentTheme();
+
+
+resetButton.on("click", () => {
+    window.location.reload();
+})
+
+
+themeSelector.trigger('change', {value: currentTheme()});
+themeSelector.on("change", (e, data) => {
+    if (data == null) return;
+    tempTheme = data.value as themes
+});
+
+
+saveButton.on("click", () => {
+    changeTheme(tempTheme);
+
+    window.location.reload();
+});
