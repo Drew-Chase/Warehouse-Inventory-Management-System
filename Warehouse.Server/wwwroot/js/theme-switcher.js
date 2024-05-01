873f9d0107157897939fd1d5e51ea44151d15db8
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-05-01 15:28:15 -0400

    Refactor theme-switching logic in theme-switcher.js
    Added proper JSDoc comments to all functions and split logic for reloading theme into two functions, 'reloadTheme' and 'currentTheme'. This makes the code more readable and maintainable. It also added "currentTheme" export.

--- Warehouse.Server/wwwroot/js/theme-switcher.js
+++ Warehouse.Server/wwwroot/js/theme-switcher.js
@@ -4,19 +4,32 @@ var themes;
     themes["light"] = "light";
 })(themes || (themes = {}));
 const link = $("link#color-theme");
+/**
+ * Changes the theme of the application.
+ *
+ * @param {themes} theme - The theme to be applied.
+ * @return {void}
+ */
 function changeTheme(theme) {
     link.attr("href", `/css/${theme}.css`);
     localStorage.setItem("theme", theme);
 }
+/**
+ * Reloads the theme according to the value stored in localStorage.
+ * If no theme is stored, the default theme (light) will be applied.
+ * @return {void}
+ */
 function reloadTheme() {
-    const theme = localStorage.getItem("theme");
-    if (theme) {
-        changeTheme(theme);
-    }
-    else {
-        changeTheme(themes.light);
-    }
+    changeTheme(currentTheme());
+}
+/**
+ * Retrieve the current theme from local storage.
+ *
+ * @returns {themes} The current theme. If not found in local storage, the default theme will be returned.
+ */
+function currentTheme() {
+    return localStorage.getItem("theme") || themes.dark;
 }
 window.onload = reloadTheme;
-export { themes, changeTheme, reloadTheme };
+export { themes, changeTheme, reloadTheme, currentTheme };
 //# sourceMappingURL=theme-switcher.js.map
--- Warehouse.Server/wwwroot/ts/theme-switcher.ts
+++ Warehouse.Server/wwwroot/ts/theme-switcher.ts
@@ -3,22 +3,37 @@
     light = "light"
 }
 
-const link:JQuery<HTMLLinkElement> = $("link#color-theme")
+const link: JQuery<HTMLLinkElement> = $("link#color-theme")
 
-function changeTheme(theme: themes) {
+/**
+ * Changes the theme of the application.
+ *
+ * @param {themes} theme - The theme to be applied.
+ * @return {void}
+ */
+function changeTheme(theme: themes): void {
     link.attr("href", `/css/${theme}.css`)
     localStorage.setItem("theme", theme)
 }
 
-function reloadTheme() {
-    const theme = localStorage.getItem("theme") as themes
-    if (theme) {
-        changeTheme(theme)
-    } else {
-        changeTheme(themes.light)
-    }
+/**
+ * Reloads the theme according to the value stored in localStorage.
+ * If no theme is stored, the default theme (light) will be applied.
+ * @return {void}
+ */
+function reloadTheme(): void {
+    changeTheme(currentTheme())
+}
+
+/**
+ * Retrieve the current theme from local storage.
+ *
+ * @returns {themes} The current theme. If not found in local storage, the default theme will be returned.
+ */
+function currentTheme(): themes {
+    return localStorage.getItem("theme") as themes || themes.dark
 }
 
 window.onload = reloadTheme
 
-export {themes, changeTheme, reloadTheme}
+export {themes, changeTheme, reloadTheme, currentTheme}
