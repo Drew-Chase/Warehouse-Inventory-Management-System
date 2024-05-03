88631bf68c36f6c412dee264ba0a7e9948db7009
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-05-03 09:56:39 -0400

    Add new PurchaseOrderModel
    Added a new structure for PurchaseOrderModel in the Warehouse Data Models. This model includes basic properties like Id, Name, Creation Date, Vendor, Buyer, Files and Status.

---
+++ Warehouse.Data/Models/PurchaseOrderModel.cs
@@ -0,0 +1,13 @@
+﻿namespace Warehouse.Data.Models;
+
+public struct PurchaseOrderModel
+{
+    public long Id { get; set; }
+    public string Name { get; set; }
+    public DateTime Created { get; set; }
+    public VendorModel[] Vendor { get; set; }
+    public BuyerModel[] Buyer { get; set; }
+    public Guid[] Files { get; set; }
+    public StatusModel[] Status { get; set; }
+
+}
