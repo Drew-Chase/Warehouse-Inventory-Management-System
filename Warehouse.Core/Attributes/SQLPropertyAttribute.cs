c7d192c9ad7a5f8196b1081a6482eab1f13d956a
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-05-03 12:52:44 -0400

    Add SQLPropertyAttribute class
    This commit introduces a new SQLPropertyAttribute class under Warehouse.Core.Attributes. The class stores information about SQL properties including the name, type, and whether it is a primary key.

---
+++ Warehouse.Core/Attributes/SQLPropertyAttribute.cs
@@ -0,0 +1,8 @@
+﻿namespace Warehouse.Core.Attributes;
+
+public class SqlPropertyAttribute(string name, string type, bool primaryKey = false) : Attribute
+{
+    public string Name { get; set; } = name;
+    public string Type { get; set; } = type;
+    public bool IsPrimaryKey { get; set; } = primaryKey;
+}
