e031bfb64d987d97be298ae32c940d2992b5f584
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-05-03 09:56:20 -0400

    Add BuyerModel to Warehouse.Data.Models
    A new BuyerModel structure has been added to the Warehouse.Data.Models namespace. This struct includes properties for Id, Name, Created, and Contacts.

---
+++ Warehouse.Data/Models/BuyerModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Warehouse.Data.Models;
+
+public struct BuyerModel
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public DateTime Created { get; set; }
+    public ContactModel[] Contacts { get; set; }
+}
