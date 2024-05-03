b57da6e97aa1234f06febeec36a329fb30b303c0
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-05-03 09:57:01 -0400

    Add VendorModel to Warehouse project
    This commit introduces a new VendorModel to the Warehouse.Data.Models namespace. The VendorModel has properties for Id, Name, Creation date, and array of ContactModels.

---
+++ Warehouse.Data/Models/VendorModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Warehouse.Data.Models;
+
+public struct VendorModel(string name, params ContactModel[] contacts)
+{
+    public Guid Id { get; set; } = Guid.NewGuid();
+    public string Name { get; set; } = name;
+    public DateTime Created { get; set; } = DateTime.Now;
+    public ContactModel[] Contacts { get; set; } = contacts;
+}
