7dca9e3046d6f0cb68eb9e4c3806d5b47117facc
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-05-03 09:56:50 -0400

    Add StatusModel to Warehouse.Data.Models
    A new struct, StatusModel, has been added to Warehouse.Data.Models namespace. This model includes properties for 'name', 'description', 'started' and 'completed', providing additional details related to the status of the warehouse data.

---
+++ Warehouse.Data/Models/StatusModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Warehouse.Data.Models;
+
+public struct StatusModel(string name, string description, DateTime? started = null, DateTime? completed = null)
+{
+    public string Name { get; set; } = name;
+    public string Description { get; set; } = description;
+    public DateTime? Started { get; set; } = started;
+    public DateTime? Completed { get; set; } = completed;
+}
