871385d8126a933aa50bade5c7ba02ff7ecfb81c
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-05-03 12:53:46 -0400

    Add PurchaseOrderManagedDatabase class
    A new class named PurchaseOrderManagedDatabase has been added to the Warehouse.SQL namespace. Inside this class, there is a static 'Initialize' method that logs the initialization of the purchase order managed database and creates an instance if it does not already exist.

---
+++ Warehouse.SQL/PurchaseOrderManagedDatabase.cs
@@ -0,0 +1,13 @@
+﻿using Serilog;
+using Warehouse.Data.Models;
+
+namespace Warehouse.SQL;
+
+public class PurchaseOrderManagedDatabase() : ManagedDatabase
+{
+    internal static void Initialize()
+    {
+        Log.Debug("Initializing the purchase order managed database.");
+        Instance.CreateIfNotExists(typeof(PurchaseOrderModel), "purchase_order");
+    }
+}
