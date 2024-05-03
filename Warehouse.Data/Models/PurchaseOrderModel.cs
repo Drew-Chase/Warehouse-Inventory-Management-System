78da33e89d72714ccd5b7dc16c9969d76994fa31
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-05-03 12:53:04 -0400

    Add SqlProperty attributes to PurchaseOrderModel
    Added SqlProperty attributes to the fields in the PurchaseOrderModel to map object fields with the database table's columns. This allows the application to correctly interact with the database, thus improving the reliability of data operations.

--- Warehouse.Data/Models/PurchaseOrderModel.cs
+++ Warehouse.Data/Models/PurchaseOrderModel.cs
@@ -1,13 +1,22 @@
-﻿namespace Warehouse.Data.Models;
+﻿using Warehouse.Core.Attributes;
+
+namespace Warehouse.Data.Models;
 
 public struct PurchaseOrderModel
 {
+    [SqlProperty("id", "BIGINT", true)]
     public long Id { get; set; }
+    [SqlProperty("name", "VARCHAR(255)")]
     public string Name { get; set; }
+    [SqlProperty("created", "DATETIME")]
     public DateTime Created { get; set; }
+    [SqlProperty("vendor", "TEXT")]
     public VendorModel[] Vendor { get; set; }
+    [SqlProperty("buyer", "TEXT")]
     public BuyerModel[] Buyer { get; set; }
+    [SqlProperty("files", "TEXT")]
     public Guid[] Files { get; set; }
+    [SqlProperty("status", "TEXT")]
     public StatusModel[] Status { get; set; }
 
 }
