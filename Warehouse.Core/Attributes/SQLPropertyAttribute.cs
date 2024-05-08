b50a06bccc848210d0409e27cf5a3f0f5c806250
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-05-08 11:49:24 -0400

    Remove SQLPropertyAttribute class
    The "SQLPropertyAttribute.cs" file was deleted because the class defined in it was no longer needed in the Warehouse Core project.

--- Warehouse.Core/Attributes/SQLPropertyAttribute.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Warehouse.Core.Attributes;
-
-public class SqlPropertyAttribute(string name, string type, bool primaryKey = false) : Attribute
-{
-    public string Name { get; set; } = name;
-    public string Type { get; set; } = type;
-    public bool IsPrimaryKey { get; set; } = primaryKey;
-}
