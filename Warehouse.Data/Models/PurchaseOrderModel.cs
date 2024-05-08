aed8e68f68d21292c6d98783adc40188c429f3e1
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-05-08 11:50:00 -0400

    Refactor PurchaseOrderModel class
    Removed SqlProperty attributes from the fields in PurchaseOrderModel class and added new fields: ShippedTo and Price. This change reduces dependencies on specific database schema while offering more flexibility.

--- Warehouse.Data/Models/PurchaseOrderModel.cs
+++ Warehouse.Data/Models/PurchaseOrderModel.cs
@@ -1,22 +1,14 @@
-﻿using Warehouse.Core.Attributes;
-
-namespace Warehouse.Data.Models;
+﻿namespace Warehouse.Data.Models;
 
 public struct PurchaseOrderModel
 {
-    [SqlProperty("id", "BIGINT", true)]
     public long Id { get; set; }
-    [SqlProperty("name", "VARCHAR(255)")]
     public string Name { get; set; }
-    [SqlProperty("created", "DATETIME")]
     public DateTime Created { get; set; }
-    [SqlProperty("vendor", "TEXT")]
     public VendorModel[] Vendor { get; set; }
-    [SqlProperty("buyer", "TEXT")]
     public BuyerModel[] Buyer { get; set; }
-    [SqlProperty("files", "TEXT")]
     public Guid[] Files { get; set; }
-    [SqlProperty("status", "TEXT")]
     public StatusModel[] Status { get; set; }
-
+    public LocationModel ShippedTo { get; set; }
+    public float Price { get; set; }
 }
