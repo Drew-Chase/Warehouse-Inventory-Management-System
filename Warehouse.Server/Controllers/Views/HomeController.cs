bae82d0e77719d337c018db85972caac3f728ddc
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-05-08 13:31:52 -0400

    Update Purchases action in HomeController
    Added multiple filter parameters to the Purchases action in HomeController. Introduced a data retrieval mechanism for locations and vendors from the database. A new record type, PurchasesFilters, was introduced to handle these parameters and is now used when returning the view.

--- Warehouse.Server/Controllers/Views/HomeController.cs
+++ Warehouse.Server/Controllers/Views/HomeController.cs
@@ -1,6 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Warehouse.Data.Models;
+using Warehouse.SQL;
 
 namespace Warehouse.Controllers.Views;
+
+public record PurchasesFilters(Status OrderStatus, LocationModel[] Locations, VendorModel[] Vendors, int Product, DateTime DateFrom, DateTime DateTo, string Query, int View, string Sort, bool Ascending);
 
 [Route("/")]
 public class HomeController : Controller
@@ -11,10 +15,22 @@ public class HomeController : Controller
     }
 
     [Route("purchases")]
-    public IActionResult Purchases()
+    public IActionResult Purchases(Status orderStatus = Status.All, string locations = "", string vendors = "", int product = 0, DateTime? dateFrom = null, DateTime? dateTo = null, string query = "", int view = 0, string sort = "date", bool ascending = true)
     {
         ViewData["title"] = "Purchases";
-        return View();
+
+        // Get locations from the database
+        LocationManager manager = new LocationManager();
+        string[] locationIds = locations.Split(',');
+        LocationModel[] remoteLocations = manager.Get().Where(i => locationIds.Contains(i.Id)).ToArray();
+
+        // Get vendors from the database
+        VendorManager vendorManager = new VendorManager();
+        string[] vendorIds = vendors.Split(',');
+        VendorModel[] remoteVendors = vendorManager.Get().Where(i => vendorIds.Contains(i.Id)).ToArray();
+
+
+        return View(new PurchasesFilters(orderStatus, remoteLocations, remoteVendors, product, dateFrom ?? DateTime.MinValue, dateTo ?? DateTime.MaxValue, query, view, sort, ascending));
     }
 
     [Route("inventory")]
