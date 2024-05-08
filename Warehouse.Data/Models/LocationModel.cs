15b0db646a9a81895e98091475c5a8d1347bdecc
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-05-08 11:49:46 -0400

    Add new LocationModel structure in Warehouse.Data.Models
    This commit introduces a new structure, named LocationModel, within the Warehouse.Data.Models namespace. This model contains properties for Id, Name, and Address which can be used to store location related information.

---
+++ Warehouse.Data/Models/LocationModel.cs
@@ -0,0 +1,8 @@
+namespace Warehouse.Data.Models;
+
+public struct LocationModel
+{
+    public string Id { get; set; }
+    public string Name { get; set; }
+    public string Address { get; set; }
+}
