5b5e0dd70de1d3af66a7b693859f5cf75f1683d5
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-05-08 11:50:18 -0400

    Change VendorModel ID type to string
    Refactored the VendorModel class to change ID type from Guid to string to better accommodate different types of ID formats that may be used.

--- Warehouse.Data/Models/VendorModel.cs
+++ Warehouse.Data/Models/VendorModel.cs
@@ -2,7 +2,7 @@
 
 public struct VendorModel(string name, params ContactModel[] contacts)
 {
-    public Guid Id { get; set; } = Guid.NewGuid();
+    public string Id { get; set; }
     public string Name { get; set; } = name;
     public DateTime Created { get; set; } = DateTime.Now;
     public ContactModel[] Contacts { get; set; } = contacts;
