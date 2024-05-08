6d14bdc862efa2cc8553c1d87690aba1a7e9d172
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-05-08 13:32:03 -0400

    Add PurchaseOrderViewComponent in Warehouse.Server
    This commit introduces the PurchaseOrderViewComponent into the Warehouse.Server. This new component handles the logic to display purchase order details when invoked. It uses the PurchaseOrderModel for data transfer.

---
+++ Warehouse.Server/ViewComponents/PurchaseOrderViewComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Warehouse.Data.Models;
+
+namespace Warehouse.ViewComponents;
+
+public class PurchaseOrderViewComponent : ViewComponent
+{
+    public IViewComponentResult Invoke(PurchaseOrderModel model)
+    {
+        return View(model);
+    }
+}
