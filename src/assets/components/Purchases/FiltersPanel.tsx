9895721835279a2355645925d8d3006d1430dec7
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-09-05 16:32:13 -0400

    Add filtering components and view toggle to FiltersPanel
    Integrated OrderStatusComponent along with added tabs to toggle between list and accordion views. Modified FilterPanel functionality to display purchase orders with refined styling and layout improvements.

--- src/assets/components/Purchases/FiltersPanel.tsx
+++ src/assets/components/Purchases/FiltersPanel.tsx
@@ -1,4 +1,40 @@
+import OrderStatusComponent from "./FilterOptions/OrderStatus/OrderStatusComponent.tsx";
+import {Divider, Tab, Tabs} from "@nextui-org/react";
+import {useState} from "react";
+import FilterByAccordionView from "./FilterByAccordionView.tsx";
+import FilterByListView from "./FilterByListView.tsx";
+import ListIcon from "../../images/icons/ListIcon.svg.tsx";
+import AccordionViewIcon from "../../images/icons/AccordionViewIcon.svg.tsx";
+
 export default function FiltersPanel()
 {
-    return (<></>);
+    const [useAccordion, setUseAccordion] = useState<boolean>(false);
+    return (
+        <div className={"flex flex-col min-w-[300px] w-1/4 h-full ml-4"}>
+            <h5>Purchase Orders</h5>
+            <div className={"flex flex-col shadow-lg bg-background-L100 min-h-[200px] w-full rounded-2xl p-4 max-h-[calc(100vh_-_160px)] h-[100vh] overflow-y-auto pr-5 mt-3"}>
+                <OrderStatusComponent/>
+                <div className={"flex flex-row items-center"}>
+                    <p className={"font-medium text-medium opacity-70 uppercase my-2 w-full"}>filter by</p>
+                    <Tabs
+                        size={"lg"}
+                        classNames={{
+                            tabList: "bg-background-L200 data-[selected=true]:text-primary",
+                            tab: "aspect-square w-[38px] h-[38px]",
+                            cursor: "!bg-primary-L-300 outline outline-2 outline-primary"
+                        }}
+                        onSelectionChange={(index) =>
+                        {
+                            setUseAccordion(index === "accordion");
+                        }}
+                    >
+                        <Tab key={"list"} title={<ListIcon size={18} fill={!useAccordion ? "hsl(var(--nextui-primary-L000))" : "hsl(var(--nextui-foreground-L-100))"}/>}/>
+                        <Tab key={"accordion"} title={<AccordionViewIcon width={24} height={18} fill={useAccordion ? "hsl(var(--nextui-primary-L000))" : "hsl(var(--nextui-foreground-L-100))"}/>}/>
+                    </Tabs>
+                </div>
+                <Divider className={"mb-4"}/>
+                {useAccordion ? <FilterByAccordionView/> : <FilterByListView/>}
+            </div>
+        </div>
+    );
 }
