1621575272dd6d32c4eb5cc820d1bda7c04ca09d
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-09-06 14:55:23 -0400

    Add new tabs and navigation for purchase order form
    Replaced the useState hook with the useNavigate hook for better navigation between tabs. Added components for different steps and updated button functionality to handle navigation accordingly. Enhanced tab styling and conditional rendering for improved UI experience.

--- src/assets/pages/AddPages/NewPurchaseOrder.tsx
+++ src/assets/pages/AddPages/NewPurchaseOrder.tsx
@@ -2,68 +2,94 @@ import {Button, Divider, Tab, Tabs} from "@nextui-org/react";
 import AddDocumentsIcon from "../../images/icons/AddDocumentsIcon.svg.tsx";
 import SaveIcon from "../../images/icons/SaveIcon.svg.tsx";
 import InformationIcon from "../../images/icons/InformationIcon.svg.tsx";
-import {useState} from "react";
+import {useNavigate} from "react-router-dom";
+import Finalize from "../../components/New Purchase Order/Finalize.tsx";
+import AdditionalInformation from "../../components/New Purchase Order/AdditionalInformation.tsx";
+import GeneralInformation from "../../components/New Purchase Order/GeneralInformation.tsx";
 
-export default function NewPurchaseOrder()
+export default function NewPurchaseOrder({tab}: { tab: string })
 {
-    const [activeTab, setActiveTab] = useState<string>("general");
+    document.title = `New Purchase Order - Warehouse`;
+    const navigate = useNavigate();
+    const nextTab = () =>
+    {
+        switch (tab)
+        {
+            case "additional":
+                navigate("/app/purchases/new/finalize");
+                break;
+            case "finalize":
+                navigate("/app/purchases/new/finalize");
+                break;
+            default:
+                navigate("/app/purchases/new/additional");
+                break;
+        }
+    };
     return (
-        <div className={"flex flex-row h-screen w-screen"}>
+        <div className={"flex flex-row h-[calc(100dvh_-_172px)] w-screen min-h-[500px] mt-8"}>
 
             <Tabs
                 placement={"start"}
                 classNames={{
+                    base: "my-auto ml-4",
                     tabList: "bg-transparent",
                     tab: "h-16 w-full",
                     cursor: "!bg-background-L200",
                     tabContent: "w-full"
                 }}
-                selectedKey={activeTab}
-                onSelectionChange={(index) => setActiveTab(index as string)}
+                defaultSelectedKey={tab}
+                onSelectionChange={(index) => navigate(`/app/purchases/new/${(index as string) === "general" ? "" : index}`)}
             >
                 <Tab
                     key={"general"}
                     title={
-                        <div className={"flex flex-row items-center text-start"}>
-                            <InformationIcon size={20} fill={activeTab === "general" ? "hsl(var(--nextui-primary-L000))" : undefined}/>
+                        <div className={"flex flex-row items-center text-start gap-2"}>
+                            <InformationIcon size={20} fill={tab === "general" ? "hsl(var(--nextui-primary-L000))" : undefined}/>
                             <div className={"flex flex-col justify-start w-full p-2"}>
                                 <p className={"opacity-50 uppercase"}>step 1</p>
-                                <p className={"capitalize"}>General Information</p>
+                                <p className={"capitalize"} style={{color: tab === "general" ? "hsl(var(--nextui-primary-L000))" : "white"}}>General Information</p>
                             </div>
                         </div>
                     }
-                />
+                >
+                    <GeneralInformation/>
+                </Tab>
                 <Tab
                     key={"additional"}
                     title={
-                        <div className={"flex flex-row items-center text-start w-full"}>
-                            <AddDocumentsIcon size={20} fill={activeTab === "additional" ? "hsl(var(--nextui-primary-L000))" : undefined}/>
+                        <div className={"flex flex-row items-center text-start gap-2"}>
+                            <AddDocumentsIcon size={20} fill={tab === "additional" ? "hsl(var(--nextui-primary-L000))" : undefined}/>
                             <div className={"flex flex-col justify-start w-full p-2"}>
                                 <p className={"opacity-50 uppercase"}>step 2</p>
-                                <p className={"capitalize"}>Additional Information</p>
+                                <p className={"capitalize"} style={{color: tab === "additional" ? "hsl(var(--nextui-primary-L000))" : "white"}}>Additional Information</p>
                             </div>
                         </div>
                     }
-                />
+                >
+                    <AdditionalInformation/>
+                </Tab>
                 <Tab
                     key={"finalize"}
                     title={
-                        <div className={"flex flex-row items-center text-start w-full"}>
-                            <SaveIcon size={20} fill={activeTab === "finalize" ? "hsl(var(--nextui-primary-L000))" : undefined}/>
+                        <div className={"flex flex-row items-center text-start gap-2"}>
+                            <SaveIcon size={20} fill={tab === "finalize" ? "hsl(var(--nextui-primary-L000))" : undefined}/>
                             <div className={"flex flex-col justify-start w-full p-2"}>
                                 <p className={"opacity-50 uppercase w-full"}>step 3</p>
-                                <p className={"capitalize w-full"}>Finalize</p>
+                                <p className={"capitalize w-full"} style={{color: tab === "finalize" ? "hsl(var(--nextui-primary-L000))" : "white"}}>Finalize</p>
                             </div>
                         </div>
                     }
-                />
+                >
+                    <Finalize/>
+                </Tab>
 
             </Tabs>
 
             <div className={"flex flex-row gap-4 items-center justify-end p-4 pr-8 fixed bottom-0 left-0 right-0 bg-background-L-100 h-[100px] border-t-3 border-t-background-L200 shadow-lg"}>
                 <Button variant={"ghost"} color={"primary"} size={"lg"}> Save as Draft </Button>
                 <Divider orientation={"vertical"}/>
-                <Button color={"primary"} size={"lg"}> Next </Button>
+                <Button color={"primary"} size={"lg"} onClick={nextTab}> {tab !== "finalize" ? "Next" : "Finish"} </Button>
             </div>
         </div>
     );
