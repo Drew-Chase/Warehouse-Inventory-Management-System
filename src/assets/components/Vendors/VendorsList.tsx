91ba4fb09df8f3bc6e6bae4fe794b7f4f9e99101
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-09-09 10:02:04 -0400

    Add VendorsList component
    Introduce a new VendorsList component to display a list of vendors with search, sorting, and view selection capabilities. Includes a button to add a new vendor and integrates necessary UI elements like inputs, dropdowns, and scrollable sections.

---
+++ src/assets/components/Vendors/VendorsList.tsx
@@ -0,0 +1,48 @@
+import {Button, cn, Divider, Input, ScrollShadow} from "@nextui-org/react";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faMagnifyingGlass} from "@fortawesome/free-solid-svg-icons";
+import SortByDropdown from "../SortByDropdown.tsx";
+import ViewSelector from "../ViewSelector.tsx";
+import VendorItem from "./VendorItem.tsx";
+import {useNavigate} from "react-router-dom";
+
+
+export default function VendorsList()
+{
+    const navigate = useNavigate();
+    return (
+        <div className={"flex flex-col w-full mx-4"}>
+            <div className="flex flex-row w-full items-center gap-4">
+                <Input
+                    classNames={{
+                        inputWrapper: cn(
+                            "bg-background-L200 outline outline-4 outline-background-L-100",
+                            "hover:outline-primary hover:outline-1 transition-all",
+                            "hover:!bg-background-L100 data-[focus=true]:!bg-background-L100"
+                        ),
+                        input: "focus:!bg-background-L100"
+                    }}
+                    label={"Search"}
+                    placeholder={"Search vendors..."}
+                    startContent={<FontAwesomeIcon icon={faMagnifyingGlass}/>}
+                />
+                <ViewSelector/>
+                <Divider orientation={"vertical"}/>
+                <SortByDropdown/>
+                <Button color={"primary"} className={"font-medium h-12"} onClick={() => navigate("/app/purchases/new")}>New Vendor</Button>
+            </div>
+            <ScrollShadow size={20} className={"flex flex-col gap-4 mt-4 max-h-[calc(100vh_-_140px)] overflow-y-auto pr-4"}>
+                {Array.from({length: 10}).map(() => (
+                    <VendorItem
+                        id={Math.floor(Math.random() * 100_000)}
+                        name={"High Desert Sales"}
+                        shortname={"HDS"}
+                        location={"123 Vendor Way. Enterprise, AL 36330"}
+                        date={new Date()}
+                        price={Math.floor(Math.random() * 100_000)}
+                    />
+                ))}
+            </ScrollShadow>
+        </div>
+    );
+}
