c0a0212b739601a33878426e54a0216ea9e1405e
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-09-09 16:25:11 -0400

    Add charts to Dashboard
    Integrated three new charts: Purchase Orders, Revenue, and Something Else. These charts utilize the Recharts library for data visualization and NextUI components for styling. This enhancement provides a streamlined view of key metrics on the Dashboard.

--- src/assets/pages/Dashboard.tsx
+++ src/assets/pages/Dashboard.tsx
@@ -1,8 +1,91 @@
+import {Card, CardBody, CardHeader} from "@nextui-org/react";
+import {Line, LineChart, Tooltip as ChartTooltip, YAxis} from "recharts";
+
 export default function Dashboard()
 {
     return (
-        <>
-            <h1>Dashboard</h1>
-        </>
+        <div className={"flex flex-col my-8"}>
+            <div className={"flex flex-row gap-4 items-center justify-center"}>
+                <Card>
+                    <CardHeader>
+                        <h5>Purchase Orders</h5>
+                    </CardHeader>
+                    <CardBody>
+                        <LineChart
+                            width={600}
+                            height={260}
+                            data={Array.from({length: 30}, (_, i) => ({name: i, po: Math.floor(Math.random() * 10)}))}
+                            className={"bg-background-L000 p-2 rounded-2xl"}
+                        >
+                            <Line type="monotone" dataKey="po" stroke="hsl(var(--nextui-primary-L000)" strokeWidth={4}/>
+                            <YAxis width={20}/>
+                            <ChartTooltip contentStyle={{
+                                backgroundColor: "hsl(var(--nextui-background-L200)",
+                                border: "none",
+                                borderRadius: "5px",
+                                boxShadow: "0 5px 10px 0 rgba(0,0,0,.5)"
+                            }}/>
+                        </LineChart>
+                    </CardBody>
+                </Card>
+                <Card>
+                    <CardHeader>
+                        <h5>Revenue</h5>
+                    </CardHeader>
+                    <CardBody>
+                        <LineChart
+                            width={600}
+                            height={260}
+                            data={Array.from({length: 30}, (_) => ({amount: Math.floor(Math.random() * 10_000)}))}
+                            className={"bg-background-L000 p-2 rounded-2xl"}
+                        >
+                            <Line type="monotone" dataKey="amount" stroke="hsl(var(--nextui-primary-L000)" strokeWidth={4}/>
+                            <YAxis width={50}/>
+                            <ChartTooltip
+                                formatter={value =>
+                                {
+                                    value = value as number;
+                                    return Intl.NumberFormat("en-US", {style: "currency", currency: "USD"}).format(value);
+                                }}
+                                contentStyle={{
+                                    backgroundColor: "hsl(var(--nextui-background-L200)",
+                                    border: "none",
+                                    borderRadius: "5px",
+                                    boxShadow: "0 5px 10px 0 rgba(0,0,0,.5)"
+                                }}/>
+                        </LineChart>
+                    </CardBody>
+                </Card>
+                <Card>
+                    <CardHeader>
+                        <h5>Something Else</h5>
+                    </CardHeader>
+                    <CardBody>
+                        <LineChart
+                            width={600}
+                            height={260}
+                            data={Array.from({length: 30}, (_) => ({amount: Math.floor(Math.random() * 10)}))}
+                            className={"bg-background-L000 p-2 rounded-2xl"}
+                        >
+                            <Line type="monotone" dataKey="amount" stroke="hsl(var(--nextui-primary-L000)" strokeWidth={4}/>
+                            <YAxis width={50}/>
+                            <ChartTooltip
+                                formatter={value =>
+                                {
+                                    value = value as number;
+                                    return Intl.NumberFormat("en-US", {style: "currency", currency: "USD"}).format(value);
+                                }}
+                                contentStyle={{
+                                    backgroundColor: "hsl(var(--nextui-background-L200)",
+                                    border: "none",
+                                    borderRadius: "5px",
+                                    boxShadow: "0 5px 10px 0 rgba(0,0,0,.5)"
+                                }}/>
+                        </LineChart>
+                    </CardBody>
+                </Card>
+            </div>
+
+        </div>
     );
 }
