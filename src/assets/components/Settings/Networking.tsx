da8979ca650a200e0b95cad5b573a701c9e7519c
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-09-11 16:04:14 -0400

    Add Networking settings component
    Created a new Networking component under the Settings section. Utilized Card elements from @nextui-org/react for the UI structure. This prepares the groundwork for future networking settings features.

---
+++ src/assets/components/Settings/Networking.tsx
@@ -0,0 +1,14 @@
+import {Card, CardBody, CardHeader} from "@nextui-org/react";
+
+export default function Networking()
+{
+    return (
+        <div className={"flex flex-col gap-4"}>
+            <Card>
+                <CardHeader><h5>Networking Settings</h5></CardHeader>
+                <CardBody>
+                </CardBody>
+            </Card>
+        </div>
+    );
+}
