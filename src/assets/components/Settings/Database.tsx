13a427691006bb526ad71ae3be3be140af3fcaac
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-09-11 16:03:47 -0400

    Add Database settings component
    Introduced a new `Database.tsx` component within the Settings directory. This component uses NextUI's Card, CardBody, and CardHeader to create a structured layout for future database settings.

---
+++ src/assets/components/Settings/Database.tsx
@@ -0,0 +1,15 @@
+import {Card, CardBody, CardHeader} from "@nextui-org/react";
+
+export default function Database()
+{
+    return (
+
+        <div className={"flex flex-col gap-4"}>
+            <Card>
+                <CardHeader><h5>Database Settings</h5></CardHeader>
+                <CardBody>
+                </CardBody>
+            </Card>
+        </div>
+    );
+}
