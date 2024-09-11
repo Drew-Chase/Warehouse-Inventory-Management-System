1624520cdad262d8e2172e41ea2011f1f220334f
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-09-11 16:04:26 -0400

    Add Profile settings component
    Introduce a new Profile component in the Settings section using NextUI's Card component. This sets up the basic structure for future enhancements and user interaction on profile-related settings.

---
+++ src/assets/components/Settings/Profile.tsx
@@ -0,0 +1,14 @@
+import {Card, CardBody, CardHeader} from "@nextui-org/react";
+
+export default function Profile()
+{
+    return (
+        <div className={"flex flex-col gap-4"}>
+            <Card>
+                <CardHeader><h5>Profile Settings</h5></CardHeader>
+                <CardBody>
+                </CardBody>
+            </Card>
+        </div>
+    );
+}
