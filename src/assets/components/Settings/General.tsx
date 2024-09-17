09f09a9895ed40023d8bdf9456405adb8605f5b9
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-09-17 16:03:49 -0400

    Refactor `General` component structure
    Replaced outer `Card` with a `div` container. Added an `id` to the `CardHeader` for better accessibility and element targeting. This change organizes the layout and enhances flexibility.

--- src/assets/components/Settings/General.tsx
+++ src/assets/components/Settings/General.tsx
@@ -5,19 +5,21 @@ import FileUploadInput from "../FileUploadInput.tsx";
 export default function General()
 {
     return (
-        <Card>
-            <CardHeader><h5>General Settings</h5></CardHeader>
-            <CardBody className={"flex flex-col gap-4"}>
-                <Input
-                    label={"Company Name"}
-                    placeholder={"Please enter the name of your company."}
-                    isRequired
-                />
-                <FileUploadInput
-                    label={"Company Logo"}
-                />
-                <Locations/>
-            </CardBody>
-        </Card>
+        <div className={"flex flex-col gap-4"}>
+            <Card>
+                <CardHeader><h5 id="general">General Settings</h5></CardHeader>
+                <CardBody className={"flex flex-col gap-4"}>
+                    <Input
+                        label={"Company Name"}
+                        placeholder={"Please enter the name of your company."}
+                        isRequired
+                    />
+                    <FileUploadInput
+                        label={"Company Logo"}
+                    />
+                    <Locations/>
+                </CardBody>
+            </Card>
+        </div>
     );
 }
