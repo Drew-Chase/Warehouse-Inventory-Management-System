48ea186fda4d379ad853952cb7660f820360cd1e
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-09-17 16:04:54 -0400

    Rename and enhance ExpandedSwitch component
    Renamed ExpandedSwitch to ExtendedSwitch and added SwitchProps inheritance. Refactored to spread remaining props and classNames, improving component flexibility and maintainability.

--- src/assets/components/Extends/ExtendedSwitch.tsx
+++ src/assets/components/Extends/ExtendedSwitch.tsx
@@ -1,6 +1,6 @@
-import {cn, Switch} from "@nextui-org/react";
+import {cn, Switch, SwitchProps} from "@nextui-org/react";
 
-interface IExpandedSwitchProps
+interface IExpandedSwitchProps extends SwitchProps
 {
     label?: string;
     description?: string;
@@ -8,26 +8,30 @@ interface IExpandedSwitchProps
     onToggle?: (selected: boolean) => void;
 }
 
-export default function ExpandedSwitch(props: IExpandedSwitchProps)
+export default function ExtendedSwitch(props: IExpandedSwitchProps)
 {
     return (
         <Switch
+            {...props}
             defaultSelected={props.selected ?? false}
-            onValueChange={props.onToggle}
+            onValueChange={props.onValueChange ?? props.onToggle}
             classNames={{
+                ...props.classNames,
                 base: cn(
                     "inline-flex flex-row-reverse w-full max-w-md bg-content1 hover:bg-background-L000 items-center",
                     "justify-between cursor-pointer rounded-lg gap-2 p-4 border-2 border-transparent",
-                    "data-[selected=true]:border-primary"
+                    "data-[selected=true]:border-primary",
+                    props.classNames?.base
                 ),
-                wrapper: "p-0 h-4 overflow-visible",
+                wrapper: cn("p-0 h-4 overflow-visible", props.classNames?.wrapper),
                 thumb: cn("w-6 h-6 border-2 shadow-lg",
                     "group-data-[hover=true]:border-primary",
                     //selected
                     "group-data-[selected=true]:ml-6",
                     // pressed
                     "group-data-[pressed=true]:w-7",
-                    "group-data-[selected]:group-data-[pressed]:ml-4"
+                    "group-data-[selected]:group-data-[pressed]:ml-4",
+                    props.classNames?.thumb
                 )
             }}
         >
