6caaf82338475f5c5865603e495e706e5b195c38
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-09-17 16:00:15 -0400

    Rearrange header elements inside PageForm component
    Moved the title and subtitle headers inside the main content div for better alignment and structure. Adjusted corresponding CSS classes to maintain consistent styling across different screen sizes.

--- src/assets/components/PageForm.tsx
+++ src/assets/components/PageForm.tsx
@@ -11,9 +11,9 @@ export default function PageForm(props: PageFormProps)
 {
     return (
         <div className={"flex flex-col w-[calc(100vw_-_300px)] min-w-[600px] items-center h-[calc(100dvh_-_170px)]"}>
-            <h5 className={"lg:text-[1.5rem] md:text-2xl sm:text-xl lg:mb-2 text-foreground-L-200/70"}>{props.title}</h5>
-            <h1 className={"lg:text-[2.5rem] md:text-3xl sm:text-2xl lg:mb-2"}>{props.subtitle}</h1>
-            <div className={"flex flex-col bg-background-L100 h-full min-h-[200px] overflow-y-auto w-10/12 rounded-t-3xl p-10 pb-10 gap-4"}>
+            <div className={"flex flex-col bg-background-L100 h-full min-h-[200px] overflow-y-auto w-[90%] rounded-t-3xl p-10 pb-10 gap-4"}>
+                <h5 className={"lg:text-[1.5rem] sm:text-xl md:text-2xl text-2xl lg:-mb-2 text-foreground-L-200/70"}>{props.title}</h5>
+                <h1 className={"lg:text-[2.5rem] sm:text-2xl md:text-3xl text-3xl lg:mb-2"}>{props.subtitle}</h1>
                 {props.children}
             </div>
         </div>
