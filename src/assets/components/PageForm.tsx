374d5de530f9a4832ae429e23a2b3c49641742c8
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-09-18 12:03:15 -0400

    Adjust PageForm layout and spacing
    Added margin to center the PageForm and top margin for better spacing. Changed the corner rounding of the inner div from 3xl to rounded-3xl to maintain a uniform styling.

--- src/assets/components/PageForm.tsx
+++ src/assets/components/PageForm.tsx
@@ -10,8 +10,8 @@ interface PageFormProps
 export default function PageForm(props: PageFormProps)
 {
     return (
-        <div className={"flex flex-col w-[calc(100vw_-_300px)] min-w-[600px] items-center h-[calc(100dvh_-_170px)]"}>
-            <div className={"flex flex-col bg-background-L100 h-full min-h-[200px] overflow-y-auto w-[90%] rounded-t-3xl p-10 pb-10 gap-4"}>
+        <div className={"flex flex-col w-[calc(100vw_-_300px)] min-w-[600px] items-center h-[calc(100dvh_-_170px)] mx-auto mt-8"}>
+            <div className={"flex flex-col bg-background-L100 h-full min-h-[200px] overflow-y-auto w-[90%] rounded-3xl p-10 pb-10 gap-4"}>
                 <h5 className={"lg:text-[1.5rem] sm:text-xl md:text-2xl text-2xl lg:-mb-2 text-foreground-L-200/70"}>{props.title}</h5>
                 <h1 className={"lg:text-[2.5rem] sm:text-2xl md:text-3xl text-3xl lg:mb-2"}>{props.subtitle}</h1>
                 {props.children}
