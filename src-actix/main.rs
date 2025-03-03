3103a2a65255500e674e8e32644c75571a3f72e2
Drew Chase (Work) <dcmanproductions@gmail.com>
2025-03-03 15:43:07 -0500

    Refactor Actix server initialization and remove legacy code
    Simplified Actix server setup by leveraging reusable modules and shared utilities. Removed redundant code for static file handling and custom request wrapping. Added proper database initialization, improved logging, and integrated Vite server for development mode.

--- src-actix/main.rs
+++ src-actix/main.rs
@@ -1,92 +1,48 @@
+#[path = "authentication/authentication.rs"]
+mod authentication;
 mod authentication_endpoint;
+mod constants;
 
-use actix_files::Files;
-use actix_files::NamedFile;
-use actix_web::error::ErrorInternalServerError;
-use actix_web::http::header;
+use crate::constants::{initialize_asset_directories, DEBUG, PORT};
+use actix_web::web::service;
 use actix_web::{
-	dev::Service as _, error, middleware, web, App, HttpResponse, HttpServer, Responder,
+    dev::Service as _, error, http::header, middleware, web, App, HttpResponse, HttpServer,
+    Responder,
 };
 use serde_json::json;
-use std::process::exit;
 
-// Function to serve the index.html file
-async fn index() -> Result<impl Responder, actix_web::Error> {
-	match NamedFile::open_async("wwwroot/index.html").await {
-		Ok(file) => Ok(file),
-		Err(_) => Err(ErrorInternalServerError("Error serving index.html")),
-	}
-}
+use anyhow::Result;
+use database_common_lib::actix_extension::create_http_server;
+use database_common_lib::database_connection::{create_pool, DatabaseConnectionData};
+use log::info;
+use vite_actix::start_vite_server;
+
 #[actix_web::main]
-async fn main() -> std::io::Result<()> {
-	std::env::set_var("RUST_LOG", "actix_web=trace");
-	env_logger::init();
+async fn main() -> Result<()> {
+    std::env::set_var("RUST_LOG", "actix_web=info");
+    env_logger::init();
+    initialize_asset_directories()?;
 
-	let port = 1420; // Port to listen on
+    // Initialize Authentication database.
+    let database_connection = DatabaseConnectionData::get().await?;
+    let pool = create_pool(&database_connection).await?;
+    authentication::initialize_authentication(&pool).await?;
 
-	println!("Starting server at http://127.0.0.1:{port}", port = port, );
+    let server = create_http_server(
+        |cfg| cfg.service(web::scope("api").configure(authentication_endpoint::configure)),
+        PORT,
+    )?;
 
-	// Initialize Authentication database
-	if authentication::init_auth().is_err() {
-		eprintln!("Failed to initialize authentication database");
-		exit(1)
-	}
+    info!(
+        "Starting {} server at http://127.0.0.1:{}",
+        if DEBUG { "development" } else { "production" },
+        PORT
+    );
 
-	HttpServer::new(move || {
-		App::new()
-			.wrap(middleware::Logger::default())
-			.wrap_fn(|req, srv| {
-				let fut = srv.call(req);
-				async {
-					let mut res = fut.await?;
-					res.headers_mut()
-					   .insert(header::ACCESS_CONTROL_ALLOW_ORIGIN, "*".parse().unwrap());
-					res.headers_mut().insert(
-						header::ACCESS_CONTROL_ALLOW_HEADERS,
-						"content-type".parse().unwrap(),
-					);
-					Ok(res)
-				}
-			})
-			.app_data(
-				web::JsonConfig::default()
-					.limit(4096)
-					.error_handler(|err, _req| {
-						let error = json!({ "error": format!("{}", err) });
-						error::InternalError::from_response(
-							err,
-							HttpResponse::BadRequest().json(error),
-						)
-							.into()
-					}),
-			)
-			// Handle API routes here
-			.service(
-				web::scope("api")
-					.service(
-						web::scope("auth")
-							.service(authentication_endpoint::create_user)
-							.service(authentication_endpoint::login)
-							.service(authentication_endpoint::login_with_token)
-							.service(authentication_endpoint::generate_access_token)
-							.service(authentication_endpoint::get_access_tokens)
-							.service(authentication_endpoint::check_if_access_token_exists)
-							.service(authentication_endpoint::get_users)
-					)
-					.service(status),
-			)
-			// Serve static files from the wwwroot directory
-			.service(Files::new("/", "wwwroot").index_file("index.html"))
-			// Handle all other routes by serving the index.html file
-			.default_service(web::route().to(index))
-	})
-		.workers(4)
-		.bind(format!("0.0.0.0:{port}", port = port))?
-		.run()
-		.await
-}
+    // Start the Vite server in development mode
+    if DEBUG {
+        start_vite_server().expect("Failed to start vite server");
+    }
 
-#[actix_web::get("/")]
-async fn status() -> impl Responder {
-	HttpResponse::Ok().json(json!({ "status": "ok" }))
+    Ok(server.await?)
 }
